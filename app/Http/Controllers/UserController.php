3fc27e224917485b61429fcc44e57fc0a6794686
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2024-02-24 02:26:08 -0300

    validacao de usuario e buscar por id

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpadteUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $users = User::paginate();
         return  UserResource::collection($users);
+    }
+
+    public function store(StoreUpadteUserRequest $request)
+    {
+        $data = $request->validated();
+        $data['password'] = bcrypt($request->password);
+
+
+        $user = User::create($data);
+
+        return new UserResource($user);
+    }
+
+
+    public function findUserById(string $id)
+    {
+        // $user = User::find($id);
+        // $user = User::where('id', '=', $id)->first();
+
+        // if (!$user) {
+        //     return response()->json([
+        //         'message' => 'User not found'
+        //     ], 404);
+        // }
+        
+        $user = User::findOrFail($id);
+        
+        
+        return new UserResource($user);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}',[UserController::class, 'findUserById']);
+Route::post('/users', [UserController::class, 'store']);
 
 Route::get('/', function () {
     return response()->json([
         'sucess' => true
     ]);
 });
+
+
+
